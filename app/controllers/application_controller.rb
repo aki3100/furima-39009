3986edb0210ce36019a8c55fae94ffea4f6b4e4d
aki3100 <velvetvoice2@gmail.com>
2023-03-09 15:49:37 +0900

    新規登録機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,12 @@
 class ApplicationController < ActionController::Base
 #  before_action :basic_auth
+#  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
 
-
-  def index
-  end
 
   private
 
-  def basic_auth
+  def basic_auth 
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
