dd370de64bba46bfd1a16f1b6be1f678a53f1481
aki3100 <velvetvoice2@gmail.com>
2023-03-10 14:14:55 +0900

    FactoryBotとテストコードを記述（nickname・email）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+
   describe "ユーザー新規登録" do
-    it "nicknameが空だと登録できない" do
+    context '新規登録できる場合' do
+      it"nicknameとemail、passwordとpassword_confirmation、名前（全角）と名前カナ（全角）と生年月日が存在すれば登録できる"do
+        expect(@user).to.be_valid
+      end
     end
-    it "emailが空では登録できない" do
+
+
+    context '新規登録できない場合' do
+      it "nicknameが空だと登録できない" do
+        @user.nickname = ""
+        @user.varid?
+        expect(@user.errors.full_messages).to include "Nickname can't be blank"
+      end
+      
+      it "emailが空では登録できない" do
+        @user.email = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Email can't be blank"
+      end
+
+      it "重複しているemailアドレスは登録できない(一意性の確認)" do
+        @user.save
+        aunther_user = build(:user, email:@user.email)
+        anather_user.valid?
+        expect(another_user.errors.full_messages).to include "Email has already been taken"
+      end
+
+      it "メールアドレスは、@を含むものでないと登録できない" do
+        @user.email = "testmail"
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Email is invalid"
+      end
+
+      it "パスワードが空だと登録できない" do
+      end
+
+      it "パスワードは6文字以上でないと登録できない" do
+      end
+
+      it "パスワードは半角英数字の混合での入力でないと登録できない" do
+      end
+
+      it "パスワードと確認用パスワードは一致していないと登録できない" do
+      end
+
+
     end
-  endend
+  end
+end
