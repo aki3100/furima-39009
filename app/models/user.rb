7ce325600330f16d48e4116f8be1d5ce153e7194
aki3100 <velvetvoice2@gmail.com>
2023-03-11 17:03:16 +0900

    Rubocopを実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,26 +5,26 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-        validates :nickname
-         #パスワードは、半角英数字混合での入力が必須であること
-        PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-        validates_format_of :password, with: PASSWORD_REGEX
+    validates :nickname
+    # パスワードは、半角英数字混合での入力が必須であること
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX
 
-        #全角（漢字・ひらがな・カタカナ）
-        validates :last_name,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-        validates :first_name,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-        #全角（カタカナ）
-        validates :last_name_kana,format: {with: /\A[ァ-ヶー]+\z/}
-        validates :first_name_kana,format: {with: /\A[ァ-ヶー]+\z/}
+    # 全角（漢字・ひらがな・カタカナ）
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    # 全角（カタカナ）
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
 
-        validates :birth_date
+    validates :birth_date
 
-        #メモ・以下はdeviseにデフォルトでバリデーション機能が実装されている
-        #・メールアドレスが必須であること。
-        #・メールアドレスが一意性であること。
-        #・メールアドレスは、@を含む必要があること。
-        #・パスワードが必須であること。
-        #・パスワードは、6文字以上での入力が必須であること。
-        #・パスワードとパスワード（確認）は、値の一致が必須であること。
+    # メモ・以下はdeviseにデフォルトでバリデーション機能が実装されている
+    # ・メールアドレスが必須であること。
+    # ・メールアドレスが一意性であること。
+    # ・メールアドレスは、@を含む必要があること。
+    # ・パスワードが必須であること。
+    # ・パスワードは、6文字以上での入力が必須であること。
+    # ・パスワードとパスワード（確認）は、値の一致が必須であること。
   end
 end
