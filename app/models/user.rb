f9e34e054df654acbeb73dbb1a8b40304fbfe1c4
aki3100 <velvetvoice2@gmail.com>
2023-03-11 16:51:00 +0900

    ユーザー管理機能のバリデーションの追記・単体テストコードを一部変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, presence: true
+  with_options presence: true do
+        validates :nickname
+         #パスワードは、半角英数字混合での入力が必須であること
+        PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+        validates_format_of :password, with: PASSWORD_REGEX
+
+        #全角（漢字・ひらがな・カタカナ）
+        validates :last_name,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+        validates :first_name,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+        #全角（カタカナ）
+        validates :last_name_kana,format: {with: /\A[ァ-ヶー]+\z/}
+        validates :first_name_kana,format: {with: /\A[ァ-ヶー]+\z/}
+
+        validates :birth_date
+
+        #メモ・以下はdeviseにデフォルトでバリデーション機能が実装されている
+        #・メールアドレスが必須であること。
+        #・メールアドレスが一意性であること。
+        #・メールアドレスは、@を含む必要があること。
+        #・パスワードが必須であること。
+        #・パスワードは、6文字以上での入力が必須であること。
+        #・パスワードとパスワード（確認）は、値の一致が必須であること。
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number:2)}
+    nickname              {"テスト"}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password.match(/[a-z\d]{8,}/i, min_length: 6)}
+    password              {"h8" + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    last_name             {"山田"}
+    first_name            {"太郎"}
+    last_name_kana        {"ヤマダ"}
+    first_name_kana       {"タロウ"}
+    birth_date            {"2023-03-10"}
   end
 end
