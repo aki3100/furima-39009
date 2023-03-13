7dfa35ae5374d4d3d49db37e43be1932110769b4
aki3100 <velvetvoice2@gmail.com>
2023-03-14 01:07:18 +0900

    itemモデルのテストコードを改修

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,6 +13,7 @@ class Item < ApplicationRecord
 
   # 空白の場合はエラーが出るようにする設定
   with_options presence: true do
+    validates :image
     validates :item_name
     validates :item_info
     validates :item_category_id
@@ -20,14 +21,13 @@ class Item < ApplicationRecord
     validates :item_shipping_fee_status_id
     validates :item_prefecture_id
     validates :item_scheduled_delivery_id
-    validates :user_id
   end
 
   # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
   validates :item_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   # ActiveHash「---」は保存できないようにする設定
-  with_options numericality: { other_then: 0 , message: "must be other_then: 0"} do
+  with_options numericality: { other_than: 0, message: "「---」以外を選んでください" } do
     validates :item_category_id
     validates :item_sales_status_id
     validates :item_shipping_fee_status_id
