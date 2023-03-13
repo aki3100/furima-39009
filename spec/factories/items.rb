6e62e896b74f5ef487891b02a282091af2d83e53
aki3100 <velvetvoice2@gmail.com>
2023-03-13 17:22:29 +0900

    FactoryBotへFaker情報・ダミーイメージを記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,18 @@
 FactoryBot.define do
   factory :item do
-    item_name                    {"商品名のテスト"}
-    item_info                    {}
-    item_category_id             {"1"}
-    item_sales_status_id         {"1"}
-    item_shipping_fee_status_id  {"1"}
-    item_prefecture_id           {"1"}
-    item_scheduled_delivery_id   {"1"}
-    item_price                   {}
-    user                         {"1"}
+    association :user
+
+    item_name                    {Faker::Name.name}
+    item_info                    {Faker::Lorem.sentence}
+    item_category_id             {Faker::Number.between(from: 1, to: 10) }
+    item_sales_status_id         {Faker::Number.between(from: 1, to: 6)} 
+    item_shipping_fee_status_id  {Faker::Number.between(from: 1, to: 11) }
+    item_prefecture_id           {Faker::Number.between(from: 1, to: 2) }
+    item_scheduled_delivery_id   {Faker::Number.between(from: 1, to: 3) }
+    item_price                   {Faker::Number.between(from: 300, to: 9999999) }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = Factory.build(:item)
+  end
+  describe '商品出品機能' do
+    context '商品が出品できる場合' do
+
+      it 'すべての情報が存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+
+      it '' do
+      end
+      it '' do
+      end
+      it '' do
+      end
+      it '' do
+      end
+      it '' do
+      end
+    end
+
+
+
+
+    context '商品が出品できない場合' do
+
+      it '' do
+      end
+      it '' do
+      end
+      it '' do
+      end
+      it '' do
+      end
+      it '' do
+      end
+    end
+  end
 end
