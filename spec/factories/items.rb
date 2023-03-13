c10b2f61c043c9b8bdc88838a591de7b82c2450b
aki3100 <velvetvoice2@gmail.com>
2023-03-13 15:47:40 +0900

    Fakerに日本語対応を実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,13 @@
 FactoryBot.define do
   factory :item do
-
+    item_name                    {"商品名のテスト"}
+    item_info                    {}
+    item_category_id             {"1"}
+    item_sales_status_id         {"1"}
+    item_shipping_fee_status_id  {"1"}
+    item_prefecture_id           {"1"}
+    item_scheduled_delivery_id   {"1"}
+    item_price                   {}
+    user                         {"1"}
+  end
 end
