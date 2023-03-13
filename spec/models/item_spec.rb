35d3d4a8b2737d933c7663bd59bd350e61186418
aki3100 <velvetvoice2@gmail.com>
2023-03-13 18:31:53 +0900

    itemモデルの正常系テストコードを記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,20 +10,31 @@ RSpec.describe Item, type: :model do
       it 'すべての情報が存在すれば登録できる' do
         expect(@item).to be_valid
       end
-
-      it '' do
+      it "カテゴリーが「---」以外であれば登録できる" do
+        @item.item_category_id = 1
+        expect(@item).to be_valid
       end
-      it '' do
+      it '商品の状態が「---」以外であれば登録できる' do
+        @item.item_sales_status_id = 1
+        expect(@item).to be_valid
       end
-      it '' do
+      it '配送料の負担が「---」以外であれば登録できる' do
+        @item.item_shipping_fee_status_id = 1
+        expect(@item).to be_valid
       end
-      it '' do
+      it '発送元の地域が「---」以外であれば登録できる' do
+        @item.item_prefecture_id = 1
+        expect(@item).to be_valid
       end
-      it '' do
+      it '発送までの日数が「---」以外であれば登録できる' do
+        @item.item_scheduled_delivery_id = 1
+        expect(@item).to be_valid
+      end
+      it '価格が半角数字で￥300~￥9,999,999以内であれば登録できる' do
+        @item.item_price = 300
+        expect(@item).to be_valid
       end
     end
-
-
 
 
     context '商品が出品できない場合' do
