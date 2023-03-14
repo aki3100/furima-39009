4616a14bd1fdd736aa3076d1cbbd5be8a942fb82
aki3100 <velvetvoice2@gmail.com>
2023-03-14 23:46:37 +0900

    配送料負担についてViewに名前の表示を出すため、id名やファイル名を変更・統一

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :item_price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :item_info, :item_category_id, :item_sales_status_id, :shopping_cost_id, :item_prefecture_id, :item_scheduled_delivery_id, :item_price).merge(user_id: current_user.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2023_03_11_104903) do
     t.text "item_info", null: false
     t.integer "item_category_id", null: false
     t.integer "item_sales_status_id", null: false
-    t.integer "item_shipping_fee_status_id", null: false
+    t.integer "shopping_cost_id", null: false
     t.integer "item_prefecture_id", null: false
     t.integer "item_scheduled_delivery_id", null: false
     t.integer "item_price", null: false
