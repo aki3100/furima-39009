1a6052986c70c59c4b47c098dbae1bcc3c83acd1
aki3100 <velvetvoice2@gmail.com>
2023-03-15 21:32:00 +0900

    FactoryBotの記述を一部訂正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     item_info                    {Faker::Lorem.sentence}
     category_id                  {Faker::Number.between(from: 1, to: 10) }
     item_status_id               {Faker::Number.between(from: 1, to: 6)} 
-    shopping_fee_status_id       {Faker::Number.between(from: 1, to: 11) }
+    shopping_cost_id             {Faker::Number.between(from: 1, to: 11) }
     prefecture_id                {Faker::Number.between(from: 1, to: 2) }
     shopping_date_id             {Faker::Number.between(from: 1, to: 3) }
     price                        {Faker::Number.between(from: 300, to: 9999999) }
