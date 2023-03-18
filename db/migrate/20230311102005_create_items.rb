ffdd72e4b766de45991d4f4c97ddf9c7bc1e50d1
aki3100 <velvetvoice2@gmail.com>
2023-03-18 20:54:59 +0900

    マイグレーションファイルを一部訂正

--- db/migrate/20230311102005_create_items.rb
+++ db/migrate/20230311102005_create_items.rb
@@ -4,10 +4,10 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string     :item_name,                   null: false
       t.text       :item_info,                   null: false
       t.integer    :category_id,                 null: false
-      t.integer    :item_status_id,             null: false
+      t.integer    :item_status_id,              null: false
       t.integer    :shopping_cost_id,            null: false
       t.integer    :prefecture_id,               null: false
-      t.integer    :shopping_date_id,       null: false
+      t.integer    :shopping_date_id,            null: false
       t.integer    :price,                       null: false
       t.references :user,                        null: false, foreign_key: true
       t.timestamps
