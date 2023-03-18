b4b00ecdabf1582f3b78f7839e7605166c1d108c
aki3100 <velvetvoice2@gmail.com>
2023-03-18 18:56:27 +0900

    Formオブジェクトにバリデーションを記述

--- app/models/order_payment.rb
+++ app/models/order_payment.rb
@@ -2,4 +2,14 @@ class OrderPayment
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
 
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :addresses
+    validates :building
+    validates :phone_number
+  
 end
