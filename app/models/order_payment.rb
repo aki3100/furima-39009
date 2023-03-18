82e7c694402b9b142b2002cc383175590d668ab4
aki3100 <velvetvoice2@gmail.com>
2023-03-18 19:27:49 +0900

    Formオブジェクトにデータ保存の記述

--- app/models/order_payment.rb
+++ app/models/order_payment.rb
@@ -11,4 +11,9 @@ class OrderPayment
     validates :addresses
     validates :phone_number, format: {with: /\A[0-9]{11}+\z/, message: "is invalid."}
   end
+
+  def save
+    order = Order.create(user_ide: user_id, item_id: item_id)
+    payment.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order: order)
+  end
 end
