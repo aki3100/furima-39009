ef2f3d95389ee0e5f36d19bb7f21af5dce8550af
aki3100 <velvetvoice2@gmail.com>
2023-03-21 17:05:15 +0900

    PAY.JP（v2）記述と環境変数の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,10 +9,11 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_payment = OrderPayment.new(order_params)
     if @order_payment.valid? 
-      @order_payment.save
-      redirect_to root_path
+       payjp_params
+       @order_payment.save
+       redirect_to root_path
     else
-      render :index
+       render :index
     end
   end
 
@@ -20,6 +21,15 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_payment).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
-    end
+  end
+
+  def payjp_params
+      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+      Payjp::Charge.create(
+        amount: @item.price,        # 商品の値段
+        card: order_params[:token], # カードトークン
+        currency: 'jpy'             # 通貨の種類（日本円）
+      )
+  end
 
 end
