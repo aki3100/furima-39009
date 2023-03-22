ca92bb5ac91e5d0c2daa59b76615aa17f5ced16a
aki3100 <velvetvoice2@gmail.com>
2023-03-22 18:54:08 +0900

    デプロイのための削除

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ module Furima39009
   class Application < Rails::Application
     config.load_defaults 6.0
 
-    Rails.application.config.i18n.default_locale = :ja
-    Faker::Config.locale = :ja
+#    Rails.application.config.i18n.default_locale = :ja
+#    Faker::Config.locale = :ja
 
   end
 end
