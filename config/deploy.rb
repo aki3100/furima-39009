e6fb44c4dd227127dcec2309161e8bfb92f77e05
aki3100 <velvetvoice2@gmail.com>
2023-03-31 18:31:16 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 
 set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/AWS-furima.pem'] 
+                                  keys: ['~/.ssh/AWS_furima.pem'] 
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
